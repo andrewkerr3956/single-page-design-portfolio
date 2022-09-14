6c9af000ce51cdf46d71e478a4440fa739d49ccd
Andrew Kerr <andrewkerr3956@gmail.com>
2022-09-13 23:42:12 -0400

    layout more of the design

--- src/assets/components/Navigation.js
+++ src/assets/components/Navigation.js
@@ -1,7 +1,11 @@
+import logo from "../logo.svg";
 const Header = () => {
     return (
         <header>
-            
+            <div className="flex">
+                <img src={logo} alt="Logo" />
+                <button className="btn btn-black" type="button" aria-label="Free Consultation">Free Consultation</button>
+            </div>
         </header>
     );
 }
