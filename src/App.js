9d7c02f229f3167c899abed2b9aa6b541c12d781
Andrew Kerr <andrewkerr3956@gmail.com>
2022-09-16 23:03:51 -0400

    desktop design work finished

--- src/App.js
+++ src/App.js
@@ -27,10 +27,10 @@ function App() {
         <section style={{ marginTop: "136px" }} name="about">
           <div className="about-flex flex-row justify-between align-center">
             <img src={AmyPicture} alt="Picture of Amy" />
-            <div style={{ width: "540px" }} className="flex-col h-75 justify-between">
+            <div className="flex-col h-75 justify-between">
               <h2>I’m Amy, and I’d love to work on your next project</h2>
               <p style={{ marginTop: "34px" }}>I love working with others to create beautiful design solutions. I’ve designed everything from brand illustrations to complete mobile apps. I’m also handy with a camera!</p>
-              <button type="button" style={{ marginTop: "32px" }} className="btn btn-lightRed w-50" aria-label="Free Consultation" label="Free Consultation">Free Consultation</button>
+              <button type="button" style={{ marginTop: "32px" }} className="btn btn-lightRed w-50" aria-label="Free Consultation">Free Consultation</button>
             </div>
           </div>
         </section>
@@ -46,9 +46,18 @@ function App() {
             </div>
           </div>
           <div className="scroll-buttons flex-row justify-center">
-              <button type="button" aria-label="Left"><img src={LeftArrowIcon} alt="Left Arrow" /></button>
-              <button type="button" aria-label="Right"><img src={RightArrowIcon} alt="Right Arrow" /></button>
+            <button type="button" aria-label="Left"><img src={LeftArrowIcon} alt="Left Arrow" /></button>
+            <button type="button" aria-label="Right"><img src={RightArrowIcon} alt="Right Arrow" /></button>
+          </div>
+        </section>
+        <section style={{ marginTop: "88px"}} name="book-call">
+          <div className="book-call-container flex-row justify-between align-center">
+            <div className="book-call-text flex-col">
+              <h2>Book a call with me</h2>
+              <p>I’d love to have a chat to see how I can help you. The best first step is for us to discuss your project during a free consultation. Then we can move forward from there.</p>
             </div>
+            <button type="button" className="btn btn-lightRed" aria-label="Free Consultation">Free Consultation</button>
+          </div>
         </section>
       </main>
       <Footer />
